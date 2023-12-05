bb6e91653cfd097ab2ceb3e5258f4aba07a2f721
Benjamin Cohen-Stead <benwcs@gmail.com>
2023-12-05 13:08:11 -0500

    v1.3.3 add spin-resolved density, current and bond corr. meas.

--- src/correlation_measurements/bond_correlation.jl
+++ src/correlation_measurements/bond_correlation.jl
@@ -42,6 +42,63 @@ function bond_correlation!(BB::AbstractArray{C,D}, b′::Bond{D}, b″::Bond{D},
                            Gdn_τ0::AbstractMatrix{T}, Gdn_0τ::AbstractMatrix{T}, Gdn_ττ::AbstractMatrix{T}, Gdn_00::AbstractMatrix{T},
                            sgn::T=one(T)) where {D, C<:Number, T<:Number}
 
+    # up-up bond-bond correlation
+    bond_correlation!(BB, b′, b″, unit_cell, lattice, Gup_τ0, Gup_0τ, Gup_ττ, Gup_00, +1, +1, sgn)
+
+    # dn-dn bond-bond correlation
+    bond_correlation!(BB, b′, b″, unit_cell, lattice, Gdn_τ0, Gdn_0τ, Gdn_ττ, Gdn_00, -1, -1, sgn)
+
+    # up-dn bond-bond correlation
+    bond_correlation!(BB, b′, b″, unit_cell, lattice, Gup_τ0, Gup_0τ, Gup_ττ, Gdn_00, +1, -1, sgn)
+
+    # dn-up bond-bond correlation
+    bond_correlation!(BB, b′, b″, unit_cell, lattice, Gdn_τ0, Gdn_0τ, Gdn_ττ, Gup_00, -1, +1, sgn)
+
+    return nothing
+end
+
+@doc raw"""
+    bond_correlation!(BB::AbstractArray{C,D}, b′::Bond{D}, b″::Bond{D}, unit_cell::UnitCell{D}, lattice::Lattice{D},
+                      Gσ′_τ0::AbstractMatrix{T}, Gσ′_0τ::AbstractMatrix{T}, Gσ′_ττ::AbstractMatrix{T}, Gσ″_00::AbstractMatrix{T},
+                      σ′::Int, σ″::Int, sgn::T=one(T)) where {D, C<:Number, T<:Number}
+
+Calculate the spin-resolved uneqaul-time bond-bond correlation function
+```math
+\begin{align*}
+    \mathcal{B}_{\mathbf{r}}^{(\mathbf{r}',a,b,\sigma'),(\mathbf{r}'',c,d,\sigma'')}(\tau)
+        = \frac{1}{N}\sum_{\mathbf{i}} & \mathcal{B}_{\mathbf{i}+\mathbf{r},\mathbf{i}}^{(\mathbf{r}',a,b,\sigma'),(\mathbf{r}'',c,d,\sigma'')}(\tau,0)\\
+        =\frac{1}{N}\sum_{\mathbf{i}} & \langle\hat{B}_{\sigma',\mathbf{i}+\mathbf{r},(\mathbf{r}',a,b)}(\tau)\hat{B}_{\sigma'',\mathbf{i},(\mathbf{r}'',c,d)}(0)\rangle,
+\end{align*}
+```
+where
+```math
+\begin{align*}
+    \hat{B}_{\sigma,\mathbf{i},(\mathbf{r},a,b)}
+        & = \hat{a}_{\sigma,\mathbf{i}+\mathbf{r}}^{\dagger}\hat{b}_{\sigma,\mathbf{i}}^{\phantom{\dagger}}+\hat{b}_{\sigma,\mathbf{i}}^{\dagger}\hat{a}_{\sigma,\mathbf{i}+\mathbf{r}}^{\phantom{\dagger}}\\
+        & = -\hat{a}_{\sigma,\mathbf{i}+\mathbf{r}}^{\phantom{\dagger}}\hat{b}_{\sigma,\mathbf{i}}^{\dagger}-\hat{b}_{\sigma,\mathbf{i}}^{\phantom{\dagger}}\hat{a}_{\sigma,\mathbf{i}+\mathbf{r}}^{\dagger}
+\end{align*}
+```
+is the bond operator.
+
+# Fields
+
+- `BB::AbstractArray{C,D}`: Array the spin-resolved bond correlation function ``\mathcal{B}_{\mathbf{r}}^{(\mathbf{r}',a,b,\sigma'),(\mathbf{r}'',c,d,\sigma'')}(\tau)`` is added to.
+- `b′::Bond{D}`: Bond defining the bond operator appearing on the left side of the bond correlation function.
+- `b″::Bond{D}`: Bond defining the bond operator appearing on the right side of the bond correlation function.
+- `unit_cell::UnitCell{D}`: Defines unit cell.
+- `lattice::Lattice{D}`: Specifies size of finite lattice.
+- `Gσ′_τ0::AbstractMatrix{T}`: The matrix ``G_{\sigma'}(\tau,0).``
+- `Gσ′_0τ::AbstractMatrix{T}`: The matrix ``G_{\sigma'}(0,\tau).``
+- `Gσ′_ττ::AbstractMatrix{T}`: The matrix ``G_{\sigma'}(\tau,\tau).``
+- `Gσ″_00::AbstractMatrix{T}`: The matrix ``G_{\sigma''}(0,0).``
+- `σ′::Int`: The electron spin appearing in the ``\hat{B}_{\sigma',\mathbf{i}+\mathbf{r},(\mathbf{r}',a,b)}`` bond operator.
+- `σ″::Int`: The electron spin appearing in the ``\hat{B}_{\sigma'',\mathbf{i},(\mathbf{r}'',c,d)}`` bond operator.
+- `sgn::T=one(T)`: The sign of the weight appearing in a DQMC simulation.
+"""
+function bond_correlation!(BB::AbstractArray{C,D}, b′::Bond{D}, b″::Bond{D}, unit_cell::UnitCell{D}, lattice::Lattice{D},
+                           Gσ′_τ0::AbstractMatrix{T}, Gσ′_0τ::AbstractMatrix{T}, Gσ′_ττ::AbstractMatrix{T}, Gσ″_00::AbstractMatrix{T},
+                           σ′::Int, σ″::Int, sgn::T=one(T)) where {D, C<:Number, T<:Number}
+
     # b′ = r′ + (r_a - r_b)
     b, a = b′.orbitals
     r′ = b′.displacement
@@ -53,65 +110,33 @@ function bond_correlation!(BB::AbstractArray{C,D}, b′::Bond{D}, b″::Bond{D},
     # zero vector
     z = @SVector zeros(Int, D)
 
-    # BB(τ,r) = BB(τ,r) + G₊(a,i+r+r′,τ|b,i+r,τ)⋅G₊(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gup_00, a, b, c, d, r′, z, r″, z, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(a,i+r+r′,τ|b,i+r,τ)⋅G₋(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gdn_00, a, b, c, d, r′, z, r″, z, 1, unit_cell, lattice, sgn)
+    # BB(τ,r) = BB(τ,r) + Gσ′(a,i+r+r′,τ|b,i+r,τ)⋅Gσ″(c,i+r″,0|d,i,0)
+    contract_Grr_G00!(BB, Gσ′_ττ, Gσ″_00, a, b, c, d, r′, z, r″, z, 1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) - G₊(c,i+r″,0|b,i+r,τ)⋅G₊(a,i+r+r′,τ|d,i,0)
-    contract_G0r_Gr0!(BB, Gup_0τ, Gup_τ0, c, b, a, d, r″, z, r′, z, -1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) - G₋(c,i+r″,0|b,i+r,τ)⋅G₋(a,i+r+r′,τ|d,i,0)
-    contract_G0r_Gr0!(BB, Gdn_0τ, Gdn_τ0, c, b, a, d, r″, z, r′, z, -1, unit_cell, lattice, sgn)
+    # BB(τ,r) = BB(τ,r) + Gσ′(a,i+r+r′,τ|b,i+r,τ)⋅Gσ″(d,i,0|c,i+r″,0)
+    contract_Grr_G00!(BB, Gσ′_ττ, Gσ″_00, a, b, d, c, r′, z, z, r″, 1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) + G₊(a,i+r+r′,τ|b,i+r,τ)⋅G₊(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gup_00, a, b, d, c, r′, z, z, r″, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(a,i+r+r′,τ|b,i+r,τ)⋅G₋(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gdn_00, a, b, d, c, r′, z, z, r″, 1, unit_cell, lattice, sgn)
+    # BB(τ,r) = BB(τ,r) + Gσ′(b,i+r,τ|a,i+r+r′,τ)⋅Gσ″(c,i+r″,0|d,i,0)
+    contract_Grr_G00!(BB, Gσ′_ττ, Gσ″_00, b, a, c, d, z, r′, r″, z, 1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) - G₊(d,i,0|b,i+r,τ)⋅G₊(a,i+r+r′,τ|c,i+r″,0)
-    contract_G0r_Gr0!(BB, Gup_0τ, Gup_τ0, d, b, a, c, z, z, r′, r″, -1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) - G₋(d,i,0|b,i+r,τ)⋅G₋(a,i+r+r′,τ|c,i+r″,0)
-    contract_G0r_Gr0!(BB, Gdn_0τ, Gdn_τ0, d, b, a, c, z, z, r′, r″, -1, unit_cell, lattice, sgn)
+    # BB(τ,r) = BB(τ,r) + Gσ′(b,i+r,τ|a,i+r+r′,τ)⋅Gσ″(d,i,0|c,i+r″,0)
+    contract_Grr_G00!(BB, Gσ′_ττ, Gσ″_00, b, a, d, c, z, r′, z, r″, 1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) + G₊(b,i+r,τ|a,i+r+r′,τ)⋅G₊(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gup_00, b, a, c, d, z, r′, r″, z, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(b,i+r,τ|a,i+r+r′,τ)⋅G₋(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gdn_00, b, a, c, d, z, r′, r″, z, 1, unit_cell, lattice, sgn)
+    # if bond correlation between same spin species
+    if σ′ == σ″
 
-    # BB(τ,r) = BB(τ,r) - G₊(c,i+r″,0|a,i+r+r′,τ)⋅G₊(b,i+r,τ|d,i,0)
-    contract_G0r_Gr0!(BB, Gup_0τ, Gup_τ0, c, a, b, d, r″, r′, z, z, -1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) - G₋(c,i+r″,0|a,i+r+r′,τ)⋅G₋(b,i+r,τ|d,i,0)
-    contract_G0r_Gr0!(BB, Gdn_0τ, Gdn_τ0, c, a, b, d, r″, r′, z, z, -1, unit_cell, lattice, sgn)
+        # BB(τ,r) = BB(τ,r) - Gσ′(c,i+r″,0|b,i+r,τ)⋅Gσ′(a,i+r+r′,τ|d,i,0)
+        contract_G0r_Gr0!(BB, Gσ′_0τ, Gσ′_τ0, c, b, a, d, r″, z, r′, z, -1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) + G₊(b,i+r,τ|a,i+r+r′,τ)⋅G₊(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gup_00, b, a, d, c, z, r′, z, r″, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(b,i+r,τ|a,i+r+r′,τ)⋅G₋(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gdn_00, b, a, d, c, z, r′, z, r″, 1, unit_cell, lattice, sgn)
+        # BB(τ,r) = BB(τ,r) - Gσ′(d,i,0|b,i+r,τ)⋅Gσ′(a,i+r+r′,τ|c,i+r″,0)
+        contract_G0r_Gr0!(BB, Gσ′_0τ, Gσ′_τ0, d, b, a, c, z, z, r′, r″, -1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) - G₊(d,i,0|a,i+r+r′)⋅G₊(b,i+r,τ|c,i+r″,0)
-    contract_G0r_Gr0!(BB, Gup_0τ, Gup_τ0, d, a, b, c, z, r′, z, r″, -1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) - G₋(d,i,0|a,i+r+r′)⋅G₋(b,i+r,τ|c,i+r″,0)
-    contract_G0r_Gr0!(BB, Gdn_0τ, Gdn_τ0, d, a, b, c, z, r′, z, r″, -1, unit_cell, lattice, sgn)
+        # BB(τ,r) = BB(τ,r) - Gσ′(c,i+r″,0|a,i+r+r′,τ)⋅Gσ′(b,i+r,τ|d,i,0)
+        contract_G0r_Gr0!(BB, Gσ′_0τ, Gσ′_τ0, c, a, b, d, r″, r′, z, z, -1, unit_cell, lattice, sgn)
 
-    # BB(τ,r) = BB(τ,r) + G₊(a,i+r+r′,τ|b,i+r,τ)⋅G₋(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gdn_00, a, b, c, d, r′, z, r″, z, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(a,i+r+r′,τ|b,i+r,τ)⋅G₊(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gup_00, a, b, c, d, r′, z, r″, z, 1, unit_cell, lattice, sgn)
-
-    # BB(τ,r) = BB(τ,r) + G₊(a,i+r+r′,τ|b,i+r,τ)⋅G₋(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gdn_00, a, b, d, c, r′, z, z, r″, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(a,i+r+r′,τ|b,i+r,τ)⋅G₊(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gup_00, a, b, d, c, r′, z, z, r″, 1, unit_cell, lattice, sgn)
-
-    # BB(τ,r) = BB(τ,r) + G₊(b,i+r,τ|a,i+r+r′,τ)⋅G₋(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gdn_00, b, a, c, d, z, r′, r″, z, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(b,i+r,τ|a,i+r+r′,τ)⋅G₊(c,i+r″,0|d,i,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gup_00, b, a, c, d, z, r′, r″, z, 1, unit_cell, lattice, sgn)
-
-    # BB(τ,r) = BB(τ,r) + G₊(b,i+r,τ|a,i+r+r′,τ)⋅G₋(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gup_ττ, Gdn_00, b, a, d, c, z, r′, z, r″, 1, unit_cell, lattice, sgn)
-    # BB(τ,r) = BB(τ,r) + G₋(b,i+r,τ|a,i+r+r′,τ)⋅G₊(d,i,0|c,i+r″,0)
-    contract_Grr_G00!(BB, Gdn_ττ, Gup_00, b, a, d, c, z, r′, z, r″, 1, unit_cell, lattice, sgn)
+        # BB(τ,r) = BB(τ,r) - Gσ′(d,i,0|a,i+r+r′,τ)⋅Gσ′(b,i+r,τ|c,i+r″,0)
+        contract_G0r_Gr0!(BB, Gσ′_0τ, Gσ′_τ0, d, a, b, c, z, r′, z, r″, -1, unit_cell, lattice, sgn)
+    end
 
     return nothing
 end
--- src/correlation_measurements/density_correlation.jl
+++ src/correlation_measurements/density_correlation.jl
@@ -54,9 +54,41 @@ function density_correlation!(DD::AbstractArray{C,D}, a::Int, b::Int, unit_cell:
     return nothing
 end
 
+@doc raw"""
+    density_correlation!(DD::AbstractArray{C,D}, a::Int, b::Int, unit_cell::UnitCell{D}, lattice::Lattice{D},
+                         Gσ_τ0::AbstractMatrix{T},  Gσ_0τ::AbstractMatrix{T},  Gσ_ττ::AbstractMatrix{T}, Gσ′_00::AbstractMatrix{T},
+                         σ′::Int, σ::Int, sgn::T=one(T)) where {D, C<:Number, T<:Number}
+
+Calculate the spin-resolved unequal-time density correlation function
+```math
+\begin{align*}
+    \mathcal{D}_{\mathbf{r}}^{(a,\sigma),(b,\sigma')}(\tau)
+            = \frac{1}{N}\sum_{\mathbf{i}} & \mathcal{D}_{\mathbf{i}+\mathbf{r},\mathbf{i}}^{(a,\sigma),(b,\sigma')}(\tau,0) \\
+            = \frac{1}{N}\sum_{\mathbf{i}} & \langle\hat{n}_{\sigma,a,\mathbf{i}+\mathbf{r}}(\tau)\hat{n}_{\sigma',b,\mathbf{r}}(0)\rangle,
+\end{align*}
+```
+where ``\hat{n}_{\sigma', b,\mathbf{i}} = \hat{b}^\dagger_{\sigma', \mathbf{i}} \hat{b}_{\sigma, \mathbf{i}}``
+is the number operator for an electron with spin ``\sigma'`` on orbital ``b`` in unit cell ``\mathbf{i}``,
+with the result being added to the array `DD`.
+
+# Fields
+
+- `DD::AbstractArray{C,D}`: The array the spin-resolved density correlation ``\mathcal{D}_{\mathbf{r}}^{(a,\sigma),(b,\sigma')}(\tau)`` is added to.
+- `a::Int`: Index specifying an orbital species in the unit cell.
+- `b::Int`: Index specifying an orbital species in the unit cell.
+- `unit_cell::UnitCell{D}`: Defines unit cell.
+- `lattice::Lattice{D}`: Specifies size of finite lattice.
+- `Gσ_τ0::AbstractMatrix{T}`: The matrix ``G_{\sigma}(\tau,0).``
+- `Gσ_0τ::AbstractMatrix{T}`: The matrix ``G_{\sigma}(0,\tau).``
+- `Gσ_ττ::AbstractMatrix{T}`: The matrix ``G_{\sigma}(\tau,\tau).``
+- `Gσ′_00::AbstractMatrix{T}`: The matrix ``G_{\sigma'}(0,0).``
+- `σ::Int`: The electron spin appearing in the ``\langle \hat{n} \rangle_{\sigma,a,\mathbf{i}+\mathbf{r}}`` density operator.
+- `σ′::Int`: The electron spin appearing in the ``\langle \hat{n} \rangle_{\sigma',b,\mathbf{i}}`` density operator.
+- `sgn::T=one(T)`: The sign of the weight appearing in a DQMC simulation.
+"""
 function density_correlation!(DD::AbstractArray{C,D}, a::Int, b::Int, unit_cell::UnitCell{D}, lattice::Lattice{D},
                               Gσ_τ0::AbstractMatrix{T},  Gσ_0τ::AbstractMatrix{T},  Gσ_ττ::AbstractMatrix{T}, Gσ′_00::AbstractMatrix{T},
-                              σ′::Int, σ::Int, sgn::T=one(T)) where {D, C<:Number, T<:Number}
+                              σ::Int, σ′::Int, sgn::T=one(T)) where {D, C<:Number, T<:Number}
 
     # define zero unit cell displacement bonds for all combos of a and b orbitals
     z = @SVector zeros(Int,D)
